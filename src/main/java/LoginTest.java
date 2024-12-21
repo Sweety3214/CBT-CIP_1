ae2b0f34ddb341fbd49750a80277b25b69d62ad3
Sweety3214 <sweetybabu3214@gmail.com>
2024-12-21 11:17:53 +0530

    docs added

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -23,6 +23,7 @@ public class LoginTest extends BrowserTest {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.urlContains("/web/index.php/dashboard/index"));
 
+
         // Validate the URL after login
         String expectedUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
         Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, "Login was not successful or redirected to the wrong page.");
